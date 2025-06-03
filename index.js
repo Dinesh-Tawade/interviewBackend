8559a587d48b2b514102fec36aecb5dc3be6d7a2
sameer <mohodvarsha22gmail.com>
2025-06-03 18:03:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+const PORT = process.env.PORT || 3000;
+
+
+export default app;
